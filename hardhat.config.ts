be483397a13b6e991f1e178eef67c5a7850d0bcd
Alexander Malyshev <alexanderem49@gmail.com>
2022-10-12 12:14:24 +0400

    Feature: updated config networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const mnemonic = process.env.MNEMONIC || "test test test test test test test test test test test junk";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    polygon: {
+      accounts: {
+        mnemonic: mnemonic
+      },
+      url: process.env.POLYGON_URL || "http://127.0.0.1"
+    },
+    mumbai: {
+      accounts: {
+        mnemonic: mnemonic
+      },
+      url: process.env.MUMBAI_URL || "http://127.0.0.1"
+    },
+  }
 };
 
 export default config;
